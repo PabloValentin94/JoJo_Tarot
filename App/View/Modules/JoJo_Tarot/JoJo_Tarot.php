4ae0d092cc4ba0ead4921434aaa50594509fd2e1
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-12-19 23:13:25 -0300

    Criando a estrutura base do projeto.

---
+++ App/View/Modules/JoJo_Tarot/JoJo_Tarot.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shotcut icon" href="/View/Modules/Images/favicon.png">
+
+        <link rel="stylesheet" type="text/css" href="/View/Modules/JoJo_Tarot/CSS/Styles.css">
+
+        <title> JoJo's Bizarre Adventure - Tarot </title>
+
+    </head>
+
+    <body>
+
+
+        
+    </body>
+
+</html>
